d17fba68b0aa765d8772bd06efc2f28ca5c928ad
Luciano Paniagua <erickmallea12345@gmail.com>
2022-04-21 10:26:17 -0400

    Algortim id3 Luciano Paniagua

---
+++ id32.py
@@ -0,0 +1,194 @@
+from math import log
+import operator
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.sans-serif'] = [u'SimHei']
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+def calcShannonEnt(dataSet):
+    """
+    Calcular la entropía de Shannon de un conjunto de datos dado
+         : param dataSet: el conjunto de datos dado
+         : return: devuelve la entropía de Shannon
+    """
+    numEntries = len(dataSet)
+    labelCounts ={}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] =0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for label in labelCounts.keys():
+        prob = float(labelCounts[label])/numEntries
+        shannonEnt -= prob*log(prob,2)
+    return shannonEnt
+
+def splitDataSet(dataSet,axis,value):
+    "" "División de conjuntos de datos según características determinadas" ""
+    retDataSet = []  # Crea un nuevo objeto de lista como los datos devueltos
+    for featVec in dataSet:
+        if featVec[axis]==value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])  # Extraer
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    "" "Elija la mejor forma de dividir el conjunto de datos" ""
+    numFeatures = len(dataSet[0])-1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        # Obtenga el i-ésimo valor característico, un valor único
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        # Calcule la entropía de información de cada método de división newEntropy
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet,i,value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob*calcShannonEnt(subDataSet)
+        # La ganancia de información es la reducción de la entropía o la reducción del desorden de datos
+        # Compare la ganancia de información en todas las características y devuelva el valor de índice de la mejor partición de características
+        infoGain = baseEntropy-newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def majorityCnt(classList):
+    "" "Obtenga el nombre de la categoría con el mejor número de apariciones" ""
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    "" "Crear árbol" ""
+    classList = [example[-1] for example in dataSet]
+    # Si las categorías son exactamente iguales, deje de seguir dividiendo
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # Devuelve la categoría más frecuente al atravesar todas las funciones
+    if len(dataSet) ==1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del labels[bestFeat]
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
+# Use anotaciones de texto para dibujar nodos de árbol
+def plotNode(nodeTxt,centerPt,parentPt,nodeType):
+    "" "Dibuja un nodo con flechas" ""
+    createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',xytext= centerPt,textcoords='axes fraction',va='center',ha='center',bbox=nodeType,arrowprops=arrow_args)
+
+def createPlot():
+    "" "Dibujar nodos de árbol" ""
+    fig = plt.figure(1,facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111,frameon=False)
+    plotNode('Nodo de decisión',(0.5,0.1),(0.1,0.5),decisionNode)
+    plotNode('Nodo de hoja',(0.8,0.1),(0.3,0.8),leafNode)
+    plt.show()
+
+def getNumLeafs(myTree):
+    "" "Obtener el número de nodos hoja" ""
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    "" "Obtener el número de niveles del árbol" ""
+    maxDepth = 0
+    firstStr =list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ =='dict':
+            thisDepth = 1+getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+def plotMidText(cntPt,parentPt,txtString):
+    "" "Complete la información de texto entre los nodos principal y secundario" ""
+    xMid = (parentPt[0]-cntPt[0])/2+cntPt[0]
+    yMid = (parentPt[1]-cntPt[1])/2+cntPt[1]
+    createPlot.ax1.text(xMid,yMid,txtString)
+
+def plotTree(myTree,parentPt,nodeTxt):
+    "" "Dibuja un diagrama de árbol" ""
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1 + float(numLeafs))/2/plotTree.totalW,plotTree.yOff)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr,cntrPt,parentPt,decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff-1/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+            plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+    plotTree.yOff = plotTree.yOff + 1 / plotTree.totalD
+
+def createPlot(inTree):
+    "" "Crear área de dibujo" ""
+    fig = plt.figure(1,facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[],yticks=[])
+    createPlot.ax1 = plt.subplot(111,frameon=False,**axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
+
+def classify(inputTree,featLabels,testVec):
+    "" "Utilice la función de clasificación del árbol de decisiones" ""
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex]==key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+def lenses_test():
+    "" "Utilice el árbol de decisiones para predecir el tipo de lentes de contacto" ""
+    fr = open('tennis.txt')
+    lenses = [inst.strip().split(',') for inst in fr.readlines()]
+    lensesLabel = ['Outlook', 'Temperature', 'Humidity', 'Windy', 'PlayTennis']
+    lensesTree = createTree(lenses,lensesLabel)  # Crear árbol
+    print(lensesTree)
+    createPlot(lensesTree)  # Dibuja el árbol
+   
+if __name__ == '__main__':
+    decisionNode = dict(boxstyle='sawtooth',fc='0.8')
+    leafNode = dict(boxstyle='round4',fc='0.8')
+    arrow_args = dict(arrowstyle='<-')
+    lenses_test()
